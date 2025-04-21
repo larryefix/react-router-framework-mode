0c5ab96c448c893544529c7c80ddf505262bf307
Larry Fix <larry.fix@got2consulting.com>
2025-04-21 11:14:32 -0500

    Change app description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
+import { Welcome } from "../welcome/welcome"
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    {
+      name: "description",
+      content: "Soon to be a React Router Framework App!",
+    },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Welcome />
 }
